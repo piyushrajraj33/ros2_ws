0b7f85e34fa476294c9a14d84f22be62799211da
Piyush Raj <piyushrajraj33@gmail.com>
2026-02-25 20:17:10 +0530

    uplaod install log

---
+++ build/arduino_diffdrive/CMakeFiles/arduino_diffdrive_node.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for arduino_diffdrive_node.
+# This may be replaced when dependencies are built.
---
+++ install/robot_bringup/share/robot_bringup/launch/robot_base.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory("robot_bringup")
+    diff_drive_config = os.path.join(pkg_path, "config", "diff_drive.yaml")
+
+    return LaunchDescription([
+
+        # Start ros2_control_node with your diff_drive.yaml
+        Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            parameters=[diff_drive_config],
+            output="screen"
+        ),
+
+        # Spawn joint_state_broadcaster first
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["joint_state_broadcaster"],
+            output="screen"
+        ),
+
+        # Spawn diff_drive_controller next
+        Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["diff_drive_controller"],
+            output="screen"
+        ),
+    ])
